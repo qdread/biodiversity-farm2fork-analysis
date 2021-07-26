31dfc5ab5824ee862e3fbe42b6ee5f7dc297a0e4
Quentin Read <quentin.read@gmail.com>
2021-07-26 13:17:25 -0400

    fix fig1 setNames

--- final_output/figs/fig1.R
+++ final_output/figs/fig1.R
@@ -59,7 +59,7 @@ totaldemand_relative <- totaldemand_sums %>%
 foodgroups_ordered <- c('grain', 'fruit', 'nuts', 'vegetables', 'sugar', 'meat/eggs', 'dairy', 'fish', 'fat')
 p_consprod_top <- ggplot(lafa_cal_summ %>% mutate(food_group = factor(food_group, levels = foodgroups_ordered)), aes(y = calories_day, x = food_group, color = diet, fill = diet, group = diet)) +
   geom_col(position = 'dodge', color = 'black', size = 0.25) +
-  scale_fill_manual(values = okabe_colors[c(1,7,3,4,5)] %>% setNames(NA), labels = diet_long_names$medium_name) +
+  scale_fill_manual(values = unname(okabe_colors[c(1,7,3,4,5)]), labels = diet_long_names$medium_name) +
   scale_y_continuous(expand = expansion(mult = c(0, 0.03)), name = 'calories per person per day') +
   scale_x_discrete(name = 'food group') +
   theme(legend.position = 'none') 
@@ -68,7 +68,7 @@ p_consprod_bottom <- ggplot(totaldemand_relative %>% filter(scenario_waste %in% 
   geom_col(aes(x = short_name, y = demand, color = scenario_diet, fill = scenario_diet, group = scenario_diet), position = 'dodge', color = 'black', size = 0.25) +
   geom_hline(yintercept = 1, linetype = 'dotted', color = 'black', size = 0.5) +
   geom_text(data = data.frame(scenario_waste = factor(c('baseline', 'allavoidable'), levels = c('baseline', 'allavoidable')), label = paste('waste scenario:', waste_long_names$long_name[c(1,4)])), aes(label = label), x = Inf, y = Inf, hjust = 1.01, vjust = 1.3) +
-  scale_fill_manual(name = 'diet', values = okabe_colors[c(1,7,3,4,5)] %>% setNames(NA), labels = diet_long_names$medium_name) +
+  scale_fill_manual(name = 'diet', values = unname(okabe_colors[c(1,7,3,4,5)]), labels = diet_long_names$medium_name) +
   facet_wrap(~ scenario_waste, nrow = 2) +
   scale_x_discrete(name = 'primary agricultural product') +
   scale_y_continuous(expand = expansion(mult = c(0, 0.03)), name = 'production relative to baseline') +

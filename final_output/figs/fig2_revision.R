ce92d81668db6db5fc5784ea834086fe4db49417
qdread <qread@sesync.org>
2021-11-22 17:31:26 -0500

    Fig 2 fix legend

--- final_output/figs/fig2_revision.R
+++ final_output/figs/fig2_revision.R
@@ -13,6 +13,45 @@ library(gridExtra)
 
 load(file.path(final_output_path, 'all_app_data.RData'))
 
+# Redo transformation of AK and HI
+county_map <- st_read(file.path(spatial_output_path, 'USA_county_2014_aea.gpkg')) %>% rename(county = fips) # High-res map
+
+rot = function(a) matrix(c(cos(a), sin(a), -sin(a), cos(a)), 2, 2)
+
+crs_lambert <- "+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"
+
+county_map_trans <- county_map %>%
+  st_transform(crs = crs_lambert)
+
+alaska <- county_map_trans %>% filter(fips_state %in% '02')
+alaska_g <- st_geometry(alaska)
+alaska_centroid <- st_centroid(st_union(alaska_g))
+
+alaska_trans <- (alaska_g - alaska_centroid) * rot(-39 * pi/180) / 2.3 + alaska_centroid + c(1000000, -5000000)
+alaska <- st_set_geometry(alaska, alaska_trans) %>% st_set_crs(st_crs(county_map_trans))
+
+
+hi_crs <- '+proj=aea +lat_1=8 +lat_2=18 +lat_0=3 +lon_0=-157 +x_0=0 +y_0=0 +ellps=WGS84 +towgs84=0,0,0,-0,-0,-0,0 +units=m +no_defs'
+hi_box <- c(xmin = -400000, ymin = 1761000, xmax = 230000, ymax = 2130000)
+
+hawaii <- county_map %>% 
+  filter(fips_state %in% '15') %>%
+  st_transform(crs = hi_crs) %>%
+  st_crop(hi_box) %>%
+  st_transform(crs = crs_lambert)
+
+hawaii_g <- st_geometry(hawaii)
+hawaii_centroid <- st_centroid(st_union(hawaii_g))
+
+hawaii_trans <- (hawaii_g - hawaii_centroid) * rot(-35 * pi/180) + hawaii_centroid + c(5200000, -1400000)
+hawaii <- st_set_geometry(hawaii, hawaii_trans) %>% st_set_crs(st_crs(county_map_trans))
+
+county_map <- county_map_trans %>%
+  filter(!fips_state %in% c('02', '15')) %>%
+  rbind(alaska) %>%
+  rbind(hawaii)
+
+
 domestic_land_toplot <- county_land_flow_sums[scenario_diet == 'baseline' & scenario_waste == 'baseline',
                                               .(VLT = sum(flow_outbound_domestic, na.rm = TRUE)),
                                               by = .(county)]
@@ -71,7 +110,7 @@ p_dext <- ggplot(left_join(county_map, domestic_ext_toplot)) +
 p_fext <- ggplot(left_join(country_map_toplot, foreign_ext_toplot)) +
   geom_sf(aes(fill = VBT), color = NA) +
   geom_sf(data = st_geometry(poly_countries), fill = NA) +
-  scale_fill_viridis_c(name = 'Foreign biodiversity\nfootprint (extinctions)', trans = 'log10', breaks = scale_breaks, labels = scale_labels, na.value = 'gray80') +
+  scale_fill_viridis_c(name = 'Foreign biodiversity\nfootprint (extinctions) ', trans = 'log10', breaks = scale_breaks, labels = scale_labels, na.value = 'gray80') +
   leg_longbottom_theme
 
 png(file.path(fp_fig, 'fig2.png'), height=8+90+90,width=8+120+120,res=400,units='mm')
@@ -81,7 +120,7 @@ grid.draw(gtable_cbind(
   gtable_rbind(ggplotGrob(p_dext), ggplotGrob(p_fext))))
 dev.off()
 
-pdf(file.path(fp_fig, 'pdfs/fig3.pdf'), height=(8+90+90)/25.4,width=(8+120+120)/25.4)
+pdf(file.path(fp_fig, 'pdfs/fig2.pdf'), height=(8+90+90)/25.4,width=(8+120+120)/25.4)
 grid.draw(gtable_cbind(
   gtable_rbind(ggplotGrob(p_dland), ggplotGrob(p_fland)),
   gtable_rbind(ggplotGrob(p_dext), ggplotGrob(p_fext))))
